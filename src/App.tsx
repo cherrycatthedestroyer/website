e9ae8a11008f0437f0c5d74e048b13b2f1ec867f
cherrycatthedestroyer <josh96j@gmail.com>
2023-07-31 18:00:09 -0700

    removed double thing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import Canvas from "./components/Canvas";
 import Loading from "./components/Loading";
-import StarField from "./scripts/starField";
-import p5Type from "p5";
 
 function App() {
   return (
