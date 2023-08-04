106fd969acb05a819d8409a51b5bb0f9f383dcc6
cherrycatthedestroyer <josh96j@gmail.com>
2023-08-03 17:06:57 -0700

    alterations to mobile version

--- src/scripts/huds/faceflute.ts
+++ src/scripts/huds/faceflute.ts
@@ -19,23 +19,28 @@ class Faceflute extends Project {
 
     show(p5: p5Type,isMobile:boolean) {
         super.show(p5,isMobile);
-        let offset;
+        let offset,offsetX,offset2,infoOffset;
         isMobile?p5.scale(1.5,1.5):p5.scale(0.9,0.9);
         isMobile?offset=120:offset=0;
+        isMobile?offsetX=-60:offsetX=0;
+        isMobile?offset2=-20:offset2=0;
+        isMobile?infoOffset=+15:infoOffset=0;
         if (this.state=="boot"||this.state=="on"||this.state=="close"){
             p5.tint(255,this.opacityCounter);
             this.logo.show(p5,-7,-220+offset,this.opacityCounter);
             this.elementCount==0? this.infoHeading.select():this.infoHeading.unselect() ;
-            this.infoHeading.show(p5,-150,-170+offset,this.opacityCounter);
+            this.infoHeading.show(p5,-150+offsetX,-170+offset+offset2,this.opacityCounter);
             this.elementCount==1? this.stackHeading.select():this.stackHeading.unselect() ;
-            this.stackHeading.show(p5,-77,-170+offset,this.opacityCounter);
+            this.stackHeading.show(p5,-77+offsetX,-170+offset+offset2,this.opacityCounter);
             this.elementCount==2? this.linkHeading.select():this.linkHeading.unselect() ;
-            this.linkHeading.show(p5,0,-170+offset,this.opacityCounter);
+            this.linkHeading.show(p5,0+offsetX,-170+offset+offset2,this.opacityCounter);
             if (this.elementCount==0){
-                this.info1.show(p5,-170,-145+offset);
+                isMobile?p5.scale(1.5,1):p5.scale(1,1);
+                this.info1.show(p5,-170+infoOffset,-145+offset+offset2);
             }
             else if (this.elementCount==1){
-                this.stack.show(p5,-170,-145+offset);
+                isMobile?p5.scale(1.5,1):p5.scale(1,1);
+                this.stack.show(p5,-170+infoOffset,-145+offset+offset2);
             }
             else if (this.elementCount==2){
                 this.link.show(p5,0,-80+offset,this.opacityCounter);
